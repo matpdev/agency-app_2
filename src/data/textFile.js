d78bbe1c6bc47ed6afa7fab654b8c274e9e647f1
matpdev <matheus2ep@gmail.com>
2021-10-12 12:27:53 -0300

    V1

---
+++ src/data/textFile.js
@@ -0,0 +1,21 @@
+export const TextFile = [
+   {
+      textHeader: "Seja Bem Vindo! Aqui é onde minha carreira começa"
+   },
+   {
+      textHeaderDown: "Pega uma pipoca e relaxe!"
+   },
+   {
+      whoisMe: [
+         {
+            apresentationTo: "Opa, eae bão? Me chamo Matheus Pereira, sou Designer Gráfico a cerca de quatro anos, e nesses quatro anos, eu vi e criei varios tipos de habilidades"
+         },
+         {
+            whatISee: "Eu criei habilidades em grupo com trabalhos com pessoas incríveis, aprendi toda parte de web e com ela hoje estou fazendo este site incrível, cheguei a níveis que nunca imaginei que iria chegar na minha vida."
+         },
+         {
+            whatIDo: "Hoje trabalho como Designer, Desenvolvedor Front-End e Freelancer de artes para mídias sociais."
+         },
+      ]
+   }
+]
