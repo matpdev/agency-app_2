5c3d36edb4537010cb81d98cce0fbb4e487737c5
matpdev <matheus2ep@gmail.com>
2021-10-13 12:59:12 -0300

    0.1.2

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,65 @@
 import React from "react";
 import "./Header.css";
-import '../Styles/hover.css'
+import "../Styles/hover.css";
 import Logo from "../../Data/logo";
 
 const Header = () => {
   return (
-    <div className="Header">
-      <div className="div">
-        <a href="#">
-          <Logo></Logo>
-        </a>
+    <>
+      <div className="Header">
+        {/* Header Logo */}
+        <div className="div">
+          <a href="#">
+            <Logo></Logo>
+          </a>
+        </div>
+        {/* Header Nav */}
+        <div className="Header-nav">
+          <ul>
+            <li>
+              <a href="#" className="hvr-grow">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hvr-grow">
+                Portifolio
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hvr-grow">
+                Biografia
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hvr-grow  ">
+                Contato
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button className="cross">M</button>
       </div>
-      <div className="Header-nav">
+      <div className="menu">
         <ul>
-          <li>
-            <a href="#" className="hvr-grow">Home</a>
-          </li>
-          <li>
-            <a href="#" className="hvr-grow">Portifolio</a>
-          </li>
-          <li>
-            <a href="#" className="hvr-grow">Biografia</a>
-          </li>
-          <li>
-            <a href="#" className="hvr-grow  ">Contato</a>
-          </li>
+          <a href="#">
+            <li>LINK ONE</li>
+          </a>
+          <a href="#">
+            <li>LINK TWO</li>
+          </a>
+          <a href="#">
+            <li>LINK THREE</li>
+          </a>
+          <a href="#">
+            <li>LINK FOUR</li>
+          </a>
+          <a href="#">
+            <li>LINK FIVE</li>
+          </a>
         </ul>
       </div>
-    </div>
+    </>
   );
 };
 
